198859256da6f2025c38b7e54cf969302ab14a97
ntsh <106670334+tffl@users.noreply.github.com>
2024-12-17 01:56:25 +0300

    feat: toNumber solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -224,8 +224,13 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const type = typeof Number(value);
+  const num = Number(value);
+  if (type === 'number' && !Number.isNaN(num)) {
+    return num;
+  }
+  return def;
 }
 
 /**
