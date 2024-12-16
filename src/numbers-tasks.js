776be39beb0019fda372f79889ca69f3683fe8a8
ntsh <106670334+tffl@users.noreply.github.com>
2024-12-17 01:51:45 +0300

    feat: isPrime solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -199,8 +199,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let flag = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      flag = false;
+    }
+  }
+  return flag;
 }
 
 /**
